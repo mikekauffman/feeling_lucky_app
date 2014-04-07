8ca65828ea5fb32352080a458cee9342b794df5e
mikekauffman <mike.a.kauffman@gmail.com>
2014-04-07 16:49:38 -0600

    fixed bug where input longer than 2 words didnt work

--- spec/lucky_app_spec.rb
+++ spec/lucky_app_spec.rb
@@ -15,9 +15,9 @@ feature 'Interact with homepage' do
 
   scenario 'User enters an input with multiple words' do
     visit '/'
-    fill_in 'lucky_input',  with: 'red sox'
+    fill_in 'lucky_input',  with: 'red sox schedule'
     click_on 'Go'
-    expect(current_url).to eq 'https://www.google.com/search?safe=on&authuser=0&biw=1056&bih=633&sclient=psy-ab&q=red+sox&oq=red+sox&gs_l=serp.9..35i39j0i67l3.3025.3674.1.6260.2.2.0.0.0.0.94.169.2.2.0.ekp%2Ckpns%3D1000...0...1.1.39.psy-ab..1.11.2363.gpxxHMp98WU&pbx=1&btnI=1'
+    expect(current_url).to eq 'https://www.google.com/search?safe=on&authuser=0&biw=1056&bih=633&sclient=psy-ab&q=red+sox+schedule&oq=red+sox+schedule&gs_l=serp.9..35i39j0i67l3.3025.3674.1.6260.2.2.0.0.0.0.94.169.2.2.0.ekp%2Ckpns%3D1000...0...1.1.39.psy-ab..1.11.2363.gpxxHMp98WU&pbx=1&btnI=1'
   end
 
 end
